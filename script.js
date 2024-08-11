b5935f5abb5f225b2a9e3660a57786fbedf922eb
Faccin27 <gfaccin27@gmail.com>
2024-08-11 18:17:40 -0300

    Adicão de novos softwares, projetos e correção de responsividade de imagem.

--- script.js
+++ script.js
@@ -1,17 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Adiciona um ouvinte de evento de clique para cada item da lista
     var listItems = document.querySelectorAll("#part li");
     listItems.forEach(function(item) {
         item.addEventListener("click", function(event) {
-            // Impede o comportamento padrão do link
             event.preventDefault();
-            // Obtém o alvo do link
             var target = event.currentTarget.querySelector("a").getAttribute("href");
-            // Obtém a seção de destino
             var section = document.querySelector(target);
-            // Verifica se a seção de destino existe
             if (section) {
-                // Rolamento suave para a seção de destino
                 section.scrollIntoView({ behavior: "smooth" });
             }
         });
